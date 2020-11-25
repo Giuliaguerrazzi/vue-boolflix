6b80883d26312e16a354cfc703b2641b0e1b3107
Giulia <giulia.guerrazzi@live.it>
2020-11-25 21:00:07 +0100

    revision and function result

--- main.js
+++ main.js
@@ -7,10 +7,18 @@ const app = new Vue({
     },
     methods: {
 
+        getResult() {
+            this.getFilms();
+            this.getSeries();
+
+            this.searchInput= '';
+        },
+
         getFilms() {
             axios.get('https://api.themoviedb.org/3/search/movie',{
                 params: {
                     api_key: 'eeb0db4d0d02bb04c55f4ea1c6ff8917',
+                    language: 'it-IT',
                     query: this.searchInput,
                 }
             })
@@ -23,13 +31,14 @@ const app = new Vue({
                 console.log(error);
             });
 
-           this.searchFilm= '';
+        
         },
 
         getSeries() {
-            axios.get('https://api.themoviedb.org/3/search/movie',{
+            axios.get('https://api.themoviedb.org/3/search/tv',{
                 params: {
                     api_key: 'eeb0db4d0d02bb04c55f4ea1c6ff8917',
+                    language: 'it-IT',
                     query: this.searchInput,
                 }
             })
@@ -37,12 +46,13 @@ const app = new Vue({
                 console.log(result.data)
 
                 this.series = result.data.results;
+                console.log(this.series);
             })
             .catch (error => {
                 console.log(error);
             });
 
-           this.searchFilm= '';
+
         },
        
         getVote(vote) {
