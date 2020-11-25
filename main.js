27d1da12a2e15b9313a500d308e1cf0dfcd679b4
Giulia <giulia.guerrazzi@live.it>
2020-11-25 13:09:49 +0100

    add function getfilms

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ const app = new Vue({
         searchFilm: '',
         films: [],
     },
-    created() {
-        axios.get('https://api.themoviedb.org/3/search/movie',{
-            params: {
-                api_key: 'eeb0db4d0d02bb04c55f4ea1c6ff8917',
-                query: 'ritorno al futuro'
-            }
-        })
-        .catch (error => {
-            console.log(error);
-        })
+    methods: {
+        getFilms() {
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: 'eeb0db4d0d02bb04c55f4ea1c6ff8917',
+                    query: this.searchFilm,
+                }
+            })
+            .then (result => {
+                console.log(result)
+
+                this.films = result.data.response;
+            })
+            .catch (error => {
+                console.log(error);
+            });
+        }
+      
     }
 }); 
