b89e036298c7f34f82d130a27bd9623d996c37d4
Giulia <giulia.guerrazzi@live.it>
2020-11-25 17:45:43 +0100

    add series

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const app = new Vue({
     },
     methods: {
 
-
-
         getFilms() {
             axios.get('https://api.themoviedb.org/3/search/movie',{
                 params: {
@@ -20,6 +18,25 @@ const app = new Vue({
                 console.log(result.data)
 
                 this.films = result.data.results;
+            })
+            .catch (error => {
+                console.log(error);
+            });
+
+           this.searchFilm= '';
+        },
+
+        getSeries() {
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: 'eeb0db4d0d02bb04c55f4ea1c6ff8917',
+                    query: this.searchInput,
+                }
+            })
+            .then (result => {
+                console.log(result.data)
+
+                this.series = result.data.results;
             })
             .catch (error => {
                 console.log(error);
