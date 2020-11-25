bfac75589040141f7f0add5d77891d2f768d0677
Giulia <giulia.guerrazzi@live.it>
2020-11-25 16:32:54 +0100

    add flag

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const app = new Vue({
     el: '#app',
     data: {
-        searchFilm: '',
+        searchInput: '',
         films: [],
+        series: [],
     },
     methods: {
+
+
+
         getFilms() {
             axios.get('https://api.themoviedb.org/3/search/movie',{
                 params: {
                     api_key: 'eeb0db4d0d02bb04c55f4ea1c6ff8917',
-                    query: this.searchFilm,
+                    query: this.searchInput,
                 }
             })
             .then (result => {
@@ -22,6 +26,10 @@ const app = new Vue({
             });
 
            this.searchFilm= '';
-        }      
+        },
+       
+        getVote(value) {
+            return value / 2;
+        }
     }
 }); 
